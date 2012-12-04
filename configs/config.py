5f0e13251a598f4c8a3fa7c19b0ca320d31d0c80
Aleksey Maksimov <ama385@maersk.com>
2012-12-04 16:22:47 +0100

    Some testing changes to config

--- configs/config.py
+++ configs/config.py
@@ -1,18 +1,20 @@
 config = [
-    {
-        'filenames': ['/home/xleap/gen/CSPS_EDU_BPM_ENG.log.1'],
-        'follow': False,
-        'type': 'obpm-log',
-        'transports': [('stdout', ), ('redis_t', 'redis://10.3.76.15:6379/0', 'logstash:obpm')],
-        # 'transports': [('file', '/tmp/outfile'), ('redis_t', )],
-        'processors': [('obpm', '<{severity}>, "{timestamp}", {engine}, {main}, {thread}, "{message}"')],
-        'tags': ['csps', 'edu', 'bpm', 'obpme1']
-    },
     # {
     #     'filenames': ['/home/xleap/gen/CSPS_EDU_BPM_ENG.log.1'],
     #     'follow': False,
-    #     'transports': [('stderr', ), ('file', '/tmp/outfile')],
+    #     'type': 'obpm-log',
+    #     'transports': [('stdout', ), ('redis_t', 'redis://10.3.76.15:6379/0', 'logstash:obpm')],
     #     # 'transports': [('file', '/tmp/outfile'), ('redis_t', )],
-    #     'processors': [('obpm', '<{severity}>, "{timestamp}", {engine}, {main}, {thread}, "{message}"')]
-    # }
+    #     'processors': [('obpm', '<{severity}>, "{timestamp}", {engine}, {main}, {thread}, "{message}"')],
+    #     'tags': ['csps', 'edu', 'bpm', 'obpme1']
+    # },
+    {
+        'filenames': ['/home/xleap/src/sawdust/dev2CTWlsMs1.log'],
+        'follow': False,
+        'type': 'weblogic-log',
+        'transports': [('stderr', ) ],
+        'tags': ['esb', 'CT', 'weblogic', 'wls'],
+        # 'transports': [('file', '/tmp/outfile'), ('redis_t', )],
+        'processors': [('passthrough', '')]
+    }
 ]
